a0b8599b0da15c80e6594e7a7470da521477bbb7
S-mon <61318055+swijckmans@users.noreply.github.com>
2025-03-17 09:21:57 +0000

    changing things slightly

--- subrequest-tester.js
+++ subrequest-tester.js
@@ -39,7 +39,7 @@ function loadExternalScript(url) {
 (async function () {
   try {
     // Replace this URL with your actual script URL
-    const scriptUrl = "https://get.phishingsites.com/viking-2.js";
+    const scriptUrl = "https://get.phishingsites.com/viking-3rd.js";
 
     console.log("Cat is making a subrequest...");
     await loadExternalScript(scriptUrl);
